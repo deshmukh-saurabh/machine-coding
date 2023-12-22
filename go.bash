ff8a695d4da8d2682272bf49f7130240fbcfac6d
Saurabh Deshmukh <saurabh.98desh@gmail.com>
2023-12-22 13:27:27 +0530

    Add files via upload

---
+++ go.bash
@@ -0,0 +1,7 @@
+# usr/env/bash
+mkdir trello_
+cd trello_
+mkdir models services
+touch main.py __init__.py
+touch models/__init__.py models/card.py models/board.py models/user.py models/list.py models/privacy.py
+touch services/__init__.py services/board_service.py services/user_service.py services/card_service.py services/list_service.py
